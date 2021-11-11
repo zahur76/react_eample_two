6c3bd0a71ecbbf946ee7cf7ab60a86528dc9135b
zahur <zahurmeerun@hotmail.com>
2021-11-11 13:21:22 +0400

    create business list component

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,5 +1,5 @@
-import './Business.css';
 import React from 'react';
+import './Business.css';
 
 const business = {
             imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
